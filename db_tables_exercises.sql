ab669b80d35df46030bcc2e219faecbd5d4682e6
Edwige Elysee <edwige.elysee@gmail.com>
2023-06-26 12:12:54 -0500

    SQL excersises 2023_06_26

---
+++ db_tables_exercises.sql
@@ -0,0 +1,38 @@
+-- 3. List all the databases
+-- SHOW DATABASES;
+
+--  4. Write the SQL code necessary to use the albums_db database
+-- USE albums_db;
+
+-- 5. Show the currently selected database
+-- SELECT DATABASE();
+
+-- 6. List all tables in the database
+-- SHOW TABLES;
+
+-- 7. Write the SQL code to switch the employees database
+-- USE employees;
+
+-- 8. Show the currently selected database
+-- SELECT DATABASE();
+
+-- 9. List all the tables in the database
+-- SHOW TABLES;
+
+-- 10. Explore the employees table. What different data types are present on this table?
+-- emp_no , birth_date , first_name , last_name , gender , hire
+
+-- 11. Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+-- salaries , emp_no
+
+-- 12. Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+-- first_name , last_name , gender
+
+-- 13. Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+-- birth_date , hire_date
+
+-- 14. What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+-- They are two seperate tables in the same databse.
+
+-- 15. Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+-- SHOW CREATE TABLE dept_manager;
