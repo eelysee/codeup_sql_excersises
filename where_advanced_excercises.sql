3ce3658227616e5f9d361b773fef962f92455f3c
Edwige Elysee <edwige.elysee@gmail.com>
2023-06-27 10:47:11 -0500

    Advanced SQL eescersises

---
+++ where_advanced_excercises.sql
@@ -0,0 +1,108 @@
+-- 1. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. 
+--What is the employee number of the top three results?
+
+SELECT * FROM employees
+WHERE first_name IN ('Irena' , 'Vidya' , 'Maya')
+
+/*
+'10200'
+'10397'
+'10610'
+*/
+
+-- 2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question?
+
+SELECT * FROM employees
+WHERE first_name = 'Irena' 
+OR first_name = 'Vidya' 
+OR first_name = 'Maya';
+
+/*
+10200
+10397
+10610
+*/
+
+-- It does not match the previos one. I have no idea why.
+
+
+-- 3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results.
+
+SELECT * FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena' 
+	OR first_name = 'Vidya' 
+	OR first_name = 'Maya');
+/* 
+10200
+10397
+10397
+*/
+
+-- 4. Find all unique last names that start with 'E'.
+
+SELECT * FROM employees
+WHERE last_name LIKE 'E%';
+
+-- 5. Find all unique last names that start or end with 'E'.
+
+SELECT DISTINCT * FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+-- 6. Find all unique last names that end with E, but does not start with E?
+
+SELECT DISTINCT * FROM employees
+WHERE last_name NOT LIKE 'E%'
+AND last_name LIKE '%E';
+
+-- 7. Find all unique last names that start and end with 'E'.
+
+SELECT DISTINCT * FROM employees
+WHERE last_name  LIKE 'E%'
+AND last_name LIKE '%E';
+
+-- 8. Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers.
+
+SELECT * FROM employees
+WHERE hire_date LIKE '199%';
+
+/*
+10008
+10011
+10012
+*/
+
+-- 9. Find all current or previous employees born on Christmas. Enter a comment with top three employee numbers.
+
+SELECT * FROM employees
+WHERE birth_date LIKE '%12-25';
+
+/*
+10078
+10115
+10261
+*/
+
+-- 10. Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with top three employee numbers.
+
+SELECT * FROM employees
+WHERE birth_date LIKE '%12-25'
+AND hire_date LIKE '199%';
+
+/*
+10261
+10438
+10681
+*/
+
+-- 11. Find all unique last names that have a 'q' in their last name.
+
+SELECT DISTINCT * FROM employees
+WHERE last_name LIKE '%q%';
+
+-- 12. Find all unique last names that have a 'q' in their last name but not 'qu'.
+
+SELECT DISTINCT * FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'; 
