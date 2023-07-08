91d56e6f8d9175fc36048d735f6804b24b781da4
Edwige Elysee <edwige.elysee@gmail.com>
2023-07-08 14:01:10 -0500

    more exercises to joins

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -20,17 +20,41 @@ LEFT JOIN employees.departments on dept_emp.dept_no = departments.dept_no
 -- a. Add a column named full_name to this table. 
 -- It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns.
 
-
+ALTER TABLE somerville_2274.employees_with_departments
+ADD full_name VARCHAR(31)
+;
 
 -- b. Update the table so that the full_name column contains the correct data.
 
+UPDATE somerville_2274.employees_with_departments
+SET full_name = CONCAT(first_name , ' ' , last_name)
+;
+
 -- c. Remove the first_name and last_name columns from the table.
+
+ALTER TABLE somerville_2274.employees_with_departments
+DROP COLUMN first_name 
+;
+
+ALTER TABLE somerville_2274.employees_with_departments
+DROP COLUMN last_name
+;
 
 -- d. What is another way you could have ended up with this same table?
 
+CREATE TEMPORARY TABLE  somerville_2274.employees_with_departments AS
+SELECT CONCAT(employees.first_name , ' ' , employees.last_name) AS full_name, departments.dept_name
+FROM employees.employees
+LEFT JOIN employees.dept_emp on employees.emp_no = dept_emp.emp_no AND dept_emp.to_date > CURDATE()
+LEFT JOIN employees.departments on dept_emp.dept_no = departments.dept_no
+;
+
 -- 2.Create a temporary table based on the payment table from the sakila database.
--- Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. 
+-- Write the SQL necessary to transform the amount column such that it is stored as an integer 
+-- representing the number of cents of the payment. 
 -- For example, 1.99 should become 199.
+
+
 
 -- 3. Go back to the employees database. Find out how the current average pay in each department compares to the overall 
 -- current pay for everyone at the company. For this comparison, you will calculate the z-score for each salary. 
@@ -38,6 +62,16 @@ LEFT JOIN employees.departments on dept_emp.dept_no = departments.dept_no
 
 -- Finding and using the z-score
 -- A z-score is a way to standardize data and compare a data point to the mean of the sample.
+/*
+    -- Returns the current z-scores for each salary
+    -- Notice that there are 2 separate scalar subqueries involved
+    SELECT salary,
+        (salary - (SELECT AVG(salary) FROM salaries where to_date > now()))
+        /
+        (SELECT stddev(salary) FROM salaries where to_date > now()) AS zscore
+    FROM salaries
+    WHERE to_date > now();
+*/
 
 -- Formula for the z-score
 -- z
